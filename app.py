9f7ad0ee5e3b08f282fdb169a700881aa797bfc4
ylws-4617 <yuluowusheng4617@gmail.com>
2018-04-11 17:25:46 -0400

    完成一大半前端脚本

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask,render_template
 import commands
 import urllib2
 import json
+import time;
 
 
 
@@ -31,6 +34,24 @@ def log_page():
 def config_page():
     return render_template("config.html")
 
+@app.route('/check_domain')
+def check_domain():
+    time.sleep(2)
+    return "true"
+
+@app.route('/get_info')
+def get_info():
+    return """{
+    "status": "off",
+    "ip": "123.123.123.123",
+    "uuid" : "dasdasd-das-d-asd-as-d-as",
+    "port" : "4567",
+    "encrypt" : "chacha20",
+    "trans" : "mkcp 伪装微信流量",
+    "tls" : "on",
+    "mux" : "on"
+    }
+    """
 
 @app.route('/get_log')
 def get_log():
