23bdbd153cde44a439d080380ee54d23e0bed09f
ylws-4617 <yuluowusheng4617@gmail.com>
2018-04-13 16:21:14 -0400

    完成基本前后端交互

--- app.py
+++ app.py
@@ -1,20 +1,81 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import commands
 import urllib2
 import json
-import time;
+import time
 
 
 
 app = Flask(__name__,static_url_path='/static')
+
+def change_config(config,value):
+    old_config = open("v2ray.config", "r")
+    old_json = json.loads(old_config.read())
+    old_config.close()
+    old_json[str(config)] = str(value)
+
+    config = open("v2ray.config","w")
+    string = str(json.dumps(old_json))
+    config.write(string)
+    config.close()
+
+
 
 def getip():
     myip = urllib2.urlopen('http://members.3322.org/dyndns/getip').read()
     myip = myip.strip()
     return str(myip)
 
+
+@app.route('/set_uuid',methods=['GET', 'POST'])
+def set_uuid():
+    items = request.args.to_dict()
+    change_config("uuid",items['setuuid'])
+    return "OK"
+
+@app.route('/set_port',methods=['GET', 'POST'])
+def set_port():
+    items = request.args.to_dict()
+    change_config("port",items['setport'])
+    return "OK"
+
+@app.route('/set_encrypt',methods=['GET', 'POST'])
+def set_encrypt():
+    items = request.args.to_dict()
+    encrypt = str(items['encrypt'])
+    if encrypt == "1":
+        change_config("encrypt","auto")
+    elif encrypt == "2":
+        change_config("encrypt", "aes-128-cfb")
+    elif encrypt == "3":
+        change_config("encrypt", "aes-128-gcm")
+    elif encrypt == "4":
+        change_config("encrypt", "chacha20-poly1305")
+    else:
+        change_config("encrypt", "none")
+
+    return "OK"
+
+@app.route('/set_trans',methods=['GET', 'POST'])
+def set_trans():
+    items = request.args.to_dict()
+    trans = str(items['trans'])
+    if trans == "1":
+        change_config("trans","tcp")
+    elif trans == "2":
+        change_config("trans", "websocket")
+    elif trans == "3":
+        change_config("trans", "mkcp")
+    elif trans == "4":
+        change_config("trans", "mkcp-srtp")
+    elif trans == "5":
+        change_config("trans", "mkcp-utp")
+    else:
+        change_config("trans","mkcp-wechat")
+
+    return "OK"
 
 
 @app.route('/')
@@ -41,17 +102,11 @@ def check_domain():
 
 @app.route('/get_info')
 def get_info():
-    return """{
-    "status": "off",
-    "ip": "123.123.123.123",
-    "uuid" : "dasdasd-das-d-asd-as-d-as",
-    "port" : "4567",
-    "encrypt" : "chacha20",
-    "trans" : "mkcp 伪装微信流量",
-    "tls" : "on",
-    "mux" : "on"
-    }
-    """
+    v2ray_config = open("v2ray.config","r")
+    json_content = json.loads(v2ray_config.read())
+    json_dump = json.dumps(json_content)
+    v2ray_config.close()
+    return json_dump
 
 @app.route('/get_log')
 def get_log():
