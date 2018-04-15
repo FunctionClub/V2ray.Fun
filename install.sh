d21f991b30d93437edbabcf3d56a0992791592f8
stack zhao <zyh001@users.noreply.github.com>
2018-04-15 17:23:14 +0800

    允许自定义登录用户名&密码

--- install.sh
+++ install.sh
@@ -60,11 +60,44 @@ cd /usr/local/V2ray.Fun/ && screen -dmS Flask python app.py
 service v2ray start
 
 ip=$(curl http://members.3322.org/dyndns/getip)
+read -p "请输入默认用户名[默认admin]： " un
+read -p "请输入默认登录密码[默认admin]： " pw
+read -p "请输入监听端口号[默认5000]： " uport
+if [[ -z "${uport}" ]];then
+	uport="5000"
+else
+	if [[ "$uport" =~ ^(-?|\+?)[0-9]+(\.?[0-9]+)?$ ]];then
+		if [[ $uport -ge "65535" || $uport -le 1 ]];then
+			echo "端口范围取值[1,65535]，应用默认端口号5000"
+			unset uport
+			uport="5000"
+		else
+			tport=`netstat -anlt | awk '{print $4}' | sed -e '1,2d' | awk -F : '{print $NF}' | sort -n | uniq | grep "$uport"`
+			if [[ ! -z ${tport} ]];then
+				echo "端口号已存在！应用默认端口号5000"
+				unset uport
+				uport="5000"
+			fi
+		fi
+	else
+		echo "请输入数字！应用默认端口号5000"
+		uport="5000"
+	fi
+fi
+if [[ -z "${un}" ]];then
+	un="admin"
+fi
+if [[ -z "${pw}" ]];then
+	pw="admin"
+fi
+sed -i "s/%%username%%/'${un}'/g" /usr/local/V2ray.Fun/panel.config
+sed -i "s/%%passwd%%/'${pw}'/g" /usr/local/V2ray.Fun/panel.config
+sed -i "s/%%port%%/'${uport}'/g" /usr/local/V2ray.Fun/panel.config
 
 echo "安装成功！"
 
-echo "面板登录地址：http://${ip}:5000"
-echo "默认用户名：admin"
-echo "默认密码：admin"
+echo "面板登录地址：http://${ip}:${uport}"
+echo "默认用户名：${un}"
+echo "默认密码：${pw}"
 echo ''
 echo "输入 v2ray 并回车可以手动管理网页面板相关功能"
