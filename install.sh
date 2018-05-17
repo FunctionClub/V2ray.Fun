f1ce1b19ebe6df3b7a6ba881fcf27b6269e27eca
雨落无声4617 <yuluowusheng4617@gmail.com>
2018-05-17 14:44:18 +0800

    修复小问题

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ git clone https://github.com/YLWS-4617/V2ray.Fun
 
 #Install Needed Python Packages
 pip install flask pyOpenSSL requests urllib3 Flask-BasicAuth supervisor
+pip install pyOpenSSL
 
 #Generate Default Configurations
 cd /usr/local/V2ray.Fun/ && python init.py
