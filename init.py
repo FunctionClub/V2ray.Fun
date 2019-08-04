3b13d680ab52bcb131f2c8ab34e2caf5f7d035f2
zhangcheng <zhangcheng@zhangchengdeMacBook-Pro.local>
2019-08-04 15:10:35 +0800

    [Fix] handel file

--- init.py
+++ init.py
@@ -4,16 +4,18 @@ import json
 import config_generator
 import uuid
 
-data = {}
 with open("/usr/local/V2ray.Fun/v2ray.config") as f:
-    data = json.load(f)
+    try:
+        data = json.load(f)
+    except ValueError:
+        data = {}
 
 data['uuid'] = str(uuid.uuid4())
 data['ip'] = config_generator.getip()
 config_generator.open_port(data['port'])
 
 with open("/usr/local/V2ray.Fun/v2ray.config", "w") as f:
-    f.write(json.dump(data))
+    json.dump(data, f)
 
 config_generator.gen_server()
 config_generator.gen_client()
